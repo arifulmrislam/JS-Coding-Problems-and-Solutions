35e1d72d56f8a201466f8835b88cb26351b822fc
arifulmrislam <ariful.mr.islam@gmail.com>
2023-11-09 13:54:07 +0600

    function

---
+++ Problems from codecademy/Function/function defination.js
@@ -0,0 +1,19 @@
+// function sayHello(name){
+//     return `Hello ${name}`;
+// }
+
+// console.log(sayHello('Mike'));
+// console.log(sayHello('Ariful'));
+// console.log(sayHello('Nusrat'));
+
+// function getGeeting() {
+//     console.log('Hello, World!');
+// }
+
+// getGeeting();
+
+// function makeShoppingList(item1='milk', item2='bread', item3='eggs'){
+//     console.log(`Remember to buy ${item1}`);
+//     console.log(`Remember to buy ${item2}`);
+//     console.log(`Remember to buy ${item3}`);
+// }
---
+++ Problems from codecademy/Function/return.js
@@ -0,0 +1,6 @@
+function monitorCount(rows, columns) {
+    return rows * columns;;
+}
+
+const numOfMonitors = monitorCount(4, 5);
+console.log(numOfMonitors);
