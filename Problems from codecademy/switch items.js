785277b05e01756e0bb8cfc73391b5af0d859487
arifulmrislam <ariful.mr.islam@gmail.com>
2023-11-09 09:43:52 +0600

    control flow

---
+++ Problems from codecademy/switch items.js
@@ -0,0 +1,34 @@
+// let groceryItem = 'papaya';
+
+// switch (groceryItem) {
+//     case 'tomato':
+//         console.log('Tomatoes are $0.49');
+//         break;
+//     case 'lame':
+//         console.log('Tomatoes are $1.49');
+//         break;
+//     case 'papaya':
+//         console.log('Tomatoes are $.29');
+//         break;
+//     default:
+//         console.log('Invalid item');
+//         break;
+//     }
+
+
+let athleteFinalPosition = 'first place';
+
+switch (athleteFinalPosition) {
+    case 'first place':
+        console.log('You get the gold medal!');
+        break;
+    case 'second place':
+        console.log('You get the silver medal!');
+        break;
+    case 'third place':
+        console.log('You get the bronze medal!');
+        break;
+    default:
+        console.log('No medal awarded.');
+        break;
+}
