ce76adc4ce65da8c07d96b2379066d9c0f231c7d
arifulmrislam <ariful.mr.islam@gmail.com>
2023-11-13 00:09:11 +0600

    updated

--- Problems from codecademy/Practice random/1.js
+++ Problems from codecademy/Practice random/1.js
@@ -8,16 +8,70 @@
 
 
 /* for ( var i = 0; i < 7; i++) {
-	setTimeout(function () {
+    setTimeout(function () {
         console.log(i);
 }, 1000);
 }
 
 for ( let i = 0; i < 7; i++) {
-	setTimeout(function () {
+    setTimeout(function () {
         console.log(i);
 }, 1000);
 }
  */
 
-console.log(math.random());
+// console.log(math.random());
+
+/* var meal = 'breakfast';
+console.log(meal);
+
+meal = 'lunch';
+console.log(meal); */
+
+
+/* var stopLight = "Yellow";
+
+if(stopLight === 'Green' && pedestrians === 0){
+    console.log('Go');
+} else {
+    console.log('stop');
+} */
+
+
+/* var sleep = true;
+console.log(!sleep); */
+
+/* let myVariable = 0;
+
+if(myVariable) {
+    console.log(myVariable);
+}else {
+    console.log('The variable does not exits');
+} */
+
+/* let numberOfApples = 1;
+
+if (numberOfApples){
+    console.log('Let us eat apples!');
+} else {
+    console.log('No apples left!');
+} */
+
+
+var isNightTime = true;
+
+if(isNightTime){
+    console.log('Time to sleep');
+}else{
+    console.log('Time to study');
+}
+
+isNightTime ? console.log('Time to sleep') :
+console.log('Time to study');
+
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase !== 'Love That!' ? 
+console.log('I love it'):
+console.log('I hate this');
