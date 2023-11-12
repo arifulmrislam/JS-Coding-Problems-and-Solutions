93187b49c1fbc07a72fd17703dff30e7f61da729
arifulmrislam <ariful.mr.islam@gmail.com>
2023-11-12 13:36:44 +0600

    updated

---
+++ Problems from codecademy/Practice random/1.js
@@ -0,0 +1,23 @@
+// var foo = "hello";
+// var bar = foo.concat(' World');
+// console.log(bar);
+
+
+// var foo = "Hello, World! How do you do?";
+// console.log(foo.indexOf(' '));
+
+
+/* for ( var i = 0; i < 7; i++) {
+	setTimeout(function () {
+        console.log(i);
+}, 1000);
+}
+
+for ( let i = 0; i < 7; i++) {
+	setTimeout(function () {
+        console.log(i);
+}, 1000);
+}
+ */
+
+console.log(math.random());
