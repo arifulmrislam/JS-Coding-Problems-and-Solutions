48b91cb6831c6e539f34b3a0a39ac50e3c8a0511
arifulmrislam <ariful.mr.islam@gmail.com>
2023-11-14 13:14:50 +0600

    loop updated

--- Problems from codecademy/Arrays/task.js
+++ Problems from codecademy/Arrays/task.js
@@ -36,3 +36,62 @@ console.log(flowers); */
 
 
 /* Nested Arrays */
+
+
+/* var vacationSpots = ['London', 'NYC', 'Dallas'];
+
+let i=0;
+for(i;i<vacationSpots.length;i++){
+    console.log('I would love to visit ' + vacationSpots[i]);
+} */
+
+
+/* const myArray = [6, 19, 20];
+const yourArray = [19, 81, 2];
+
+for(let i=0;i<myArray.length;i++){
+    for(let j=0;j<yourArray.length;j++){
+        if(myArray[i] === yourArray[j]){
+            console.log('Both arrays have the numbers ' + yourArray[j]);
+        }
+    }
+} */
+
+
+/* const bobsFollowers = ['Arif', 'Nusrat', 'Jamal', 'Kamal'];
+const tinasFollowers = ['Arif', 'Nusrat', 'Mahbub'];
+const mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+    for (let j = 0; j < tinasFollowers.length; j++) {
+        if (bobsFollowers[i] === tinasFollowers[j]) {
+            mutualFollowers.push(tinasFollowers[j]);
+        }
+    }
+}
+console.log(mutualFollowers); */
+
+
+
+/* for(let counterOne=1;counterOne <4; counterOne++){
+    console.log(counterOne);
+}
+
+let counterTwo = 1;
+while(counterTwo <4){
+    console.log(counterTwo);
+    counterTwo++;
+} */
+
+
+
+const firstMessage = 'I will print';
+const secondMessage = 'I will not print';
+
+do {
+    console.log(firstMessage)
+} while(true === false);
+
+while(true === false){
+    console.log(secondMessage);
+}
