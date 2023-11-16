cb5a7a9ab928c6ca7604982e77a728dbd3047eed
arifulmrislam <ariful.mr.islam@gmail.com>
2023-11-16 14:53:16 +0600

    JS updated

---
+++ Problems from codecademy/Practice random/Data type prob/1.js
@@ -0,0 +1,55 @@
+/* let numeric = 100;
+let string = 'Think more about future goal and study';
+let boolean = true;
+
+console.log(typeof numeric);
+console.log(typeof string);
+console.log(typeof boolean); */
+
+/* //Global variables
+var sum1=10;
+var sum2=20;
+
+//Function that accesses global variables
+function add(){
+    return sum1 + sum2;
+    
+}
+
+//Call the function and log the result
+const sum = add();
+console.log(sum); */
+
+// function blockScopeExample() {
+//     let localVar = 'I am local';
+
+
+//     for (let i = 0; i <= 3; i++) {
+//         console.log('Inside the for loop: ', i);
+//         console.log('Accessing localVar inside the for loop:', localVar);
+// }
+
+//     console.log('Accessing localVar outside the for loop: ', localVar);
+// }
+
+// blockScopeExample();
+
+
+
+/* let person = {
+    'name': 'Ariful Islam',
+    'age': 28,
+    'profession': 'Software Engineer'
+};
+
+console.log(person.name);
+console.log(person.age); */
+
+// let dynamicVariable = 42;
+
+// console.log('Initial Type', typeof dynamicVariable);
+
+
+// dynamicVariable = 'Hello, World!';
+
+// console.log('Updated Type', typeof dynamicVariable);
