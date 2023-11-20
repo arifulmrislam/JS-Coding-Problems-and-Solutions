7682add0c9dfdd803f0a87780d6c99a0a86387a2
arifulmrislam <ariful.mr.islam@gmail.com>
2023-11-20 19:28:26 +0600

    loop

--- Problems from codecademy/Loops/1.js
+++ Problems from codecademy/Loops/1.js
@@ -15,10 +15,78 @@
 // // keep this function call here 
 // console.log(SimpleEvens(10));
 
-function WordCount(str) {
+/* function WordCount(str) {
     // Split the string into an array of words using spaces as the delimiter
     const words = str.split(' ');
 
     // Return the number of words in the array
     return words.length;
+} */
+
+
+
+/* let num=1;
+while(num<=10){
+    console.log(num);
+    num++;
+} */
+
+
+
+
+// let x = 0;
+// let i = 0;
+
+// do{
+//     x = x + i;
+//     console.log(x);
+//     i++;
+// }while(i < 5);
+
+
+
+
+/* const items = {
+    apple: 2, 
+    banana: 5, 
+    cherry: 0,
+};
+
+for (const item in items) {
+console.log(items[item]);
+} */
+
+
+
+/* Reverse array */
+/* const item2 = ['apricot', 'banana', 'cherry'];
+
+const item3 = item2.slice().reverse();
+
+console.log(item3); */
+
+/* const objectK = {
+    name: 'Codecademy',
+    age: 10,
+};
+for(var key in objectK){
+    console.log(`${key}:`,objectK[key]);
+}
+
+const myEntires = Object.entries(objectK);
+console.log(myEntires); */
+// const myEntires = Object.entries(objectK);
+// console.log(myEntires);
+
+
+// for(var key in objectK){
+//     console.log(`${key}:`,objectK[key]);
+// }
+
+
+for (let i = 0; i < 99; i += 1) {
+    if (i > 5) {
+        break;
+    }
+    console.log(i);
 }
---
+++ Problems from codecademy/Loops/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const items = {
+    apple: 2, 
+    banana: 5, 
+    cherry: 0,
+};
+
+for (const item in items) {
+console.log(items[item]);
+}
