2f96209cb139766b6c2d066125b1ed927a444beb
arifulmrislam <ariful.mr.islam@gmail.com>
2024-06-19 11:56:22 +0600

    🔥

---
+++ JavaScript Refresher/js_refresher.js
@@ -0,0 +1,91 @@
+"use strict";
+/*
+for(var i = 0; i < 7; ++i){
+    setTimeout(function(){
+        console.log(i);
+    },1000);
+}
+
+for(let i = 0; i < 7; ++i){
+    setTimeout(function(){
+        console.log(i);
+    },1000);
+}
+
+
+const myObj = {
+    foo: 'bar'
+};
+
+myObj.foo = 42;
+myObj.home = 'Dhaka';
+
+console.log(myObj);
+
+
+let planet = 'Mercury';
+
+switch (planet){
+    case 'Mercury':
+    case 'Venus':
+        console.log('Too hot here.');
+    case 'Earth':
+        console.log('Welcome home!');
+        break;
+    case 'Mars':
+        console.log('Welcome to the other home!');
+        break;
+    case 'Jupiter':
+    case 'Saturn':
+    case 'Uranus':
+    case 'Neptune':
+    case 'Pluto':
+        console.log('You may get gold here.');
+        break;
+
+    default:
+        console.log('Seems you found another planet.');
+        break;
+}
+
+
+for(var i=0;i<=10;i++){
+    console.log(i);
+}
+
+var name = {
+    first: 'Johnny',
+    last: 'B.'
+};
+
+for(var key in name){
+    if(name.hasOwnProperty(key)){
+        console.log(key, name[key]);
+    }
+}
+
+
+let numbers = [-1, 7, 42, 64];
+for(let num of numbers){
+    console.log(num);
+}
+
+
+var country = 'Bangladesh';
+for(let letter of country){
+    console.log(letter);
+}
+
+let i = 1;
+while(i <= 10){
+    console.log(i);
+    i++;
+}
+
+var i = 0;
+
+do{
+    ++i;
+    console.log(i);
+}while(i < 10);
+*/
